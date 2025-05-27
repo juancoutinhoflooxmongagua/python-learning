588ac3265cfe8497aab7da4d8ca558ac0d52dd52
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-27 14:09:40 -0300

    feat: variables

---
+++ Variables/aula7.py
@@ -0,0 +1,12 @@
+
+# Variables
+name = 'Juan'
+last_name = 'Coutinho'
+age = 18
+majority = age >= 18 
+sum = 1 + 1
+
+complete_name = name + last_name
+
+print("nome completo:", complete_name, age, "é maior de idade? ", majority)
+
