c8fd7440c5c2d442ec27b8b1ffe3efdeb003cfee
JuanCoutinho <juan.devx@gmail.com>
2025-05-27 10:10:56 -0300

    feat: string of text

--- aula3.py
+++ aula3.py
@@ -1,1 +1,20 @@
 print("aula 3 - py ")
+
+"""
+    String de texto 
+
+    using - "" - and  - '' -     
+
+"""
+
+# Aspas
+
+print('String de aspas simples')
+print("String de aspas duplas")
+
+# Escape
+
+print("String de aspas \"duplas\" ")
+
+# R
+print(r"String de aspas \"duplas\" ")
