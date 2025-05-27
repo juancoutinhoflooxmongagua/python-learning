62ba18b329722547824dd06e8f6adf316368e41a
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-27 15:03:09 -0300

    feat: Login with If Else

---
+++ Operators/aula17.py
@@ -0,0 +1,12 @@
+
+
+user_password = '1234'
+
+password_local = input('Digite sua senha: ')
+
+if password_local == user_password:
+    print('Bem vindo ao sistema')
+elif password_local != user_password: 
+    print('Senha incorreta')
+    
+
