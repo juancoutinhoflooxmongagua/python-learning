6a10ee115f54134d61dfb5b007c5ce346765cc95
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-27 14:50:13 -0300

    feat: Input method

---
+++ Operators/aula15.py
@@ -0,0 +1,11 @@
+name = input('Qual o seu nome? ')
+
+print(f'Seu nome é {name}')
+
+n1 = input('Digite o primeiro valor')
+n2 = input('Digite o segundo valor')
+
+int_n1 = int(n1)
+int_n2 = int(n2)
+
+print(f'o Resultado da soma é {int_n1 + int_n2}')
