4e6b4fffb98fe29433404d50dcb28d8cd9c25cbe
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-27 14:56:00 -0300

    feat: If, Else and Ellif

---
+++ Operators/aula16.py
@@ -0,0 +1,8 @@
+entry = input('You want to enter to the system? (yes/no): ')
+
+if entry == "yes":
+    print('You entered to the system')
+elif entry == "no":
+    print('You back to the system')
+else: 
+    print('Error')
