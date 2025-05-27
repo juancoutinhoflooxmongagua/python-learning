2db41e20c470964add3c546fd5c8790b24ea4f34
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-27 14:19:52 -0300

    feat: Operators

---
+++ Operators/aula9.py
@@ -0,0 +1,15 @@
+n1 = 5
+
+sum = n1 + n1
+division = n1 / n1
+multiplication = n1 * n1
+subtraction = n1 - n1
+
+print(
+    "Ficha:",
+    "Resultados usando o número:", n1,
+    "Soma: ", sum,
+    "Divisão: ", division,
+    "Multiplicação: ", multiplication,
+    "Subtração: ", subtraction
+)
