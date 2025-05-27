3ea7d9102b3e61e846d92810545d3a771694fb5a
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-27 14:10:50 -0300

    feat: finish exercise

--- Variables/aula7.py
+++ Variables/aula7.py
@@ -4,9 +4,10 @@ name = 'Juan'
 last_name = 'Coutinho'
 age = 18
 majority = age >= 18 
+height = 1.80
 sum = 1 + 1
 
 complete_name = name + last_name
 
-print("nome completo:", complete_name, age, "é maior de idade? ", majority)
+print("nome completo:", complete_name, age, "é maior de idade? ", majority, "e tem", height,"metros")
 
