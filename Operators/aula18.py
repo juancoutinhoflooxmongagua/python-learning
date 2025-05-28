17951aa6a0c156983c364759c77f2f2f1aa00a3c
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-28 08:54:38 -0300

    feat: interpreter and debugger

---
+++ Operators/aula18.py
@@ -0,0 +1,20 @@
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('código para condição 1')
+elif condicao2:
+    print('código para condição 2')
+elif condicao3:
+    print('código para condição 3')
+elif condicao4:
+    print('código para condição 4')
+else:
+    print('nenhuma condição foi satisfeita.')
+if 10 == 10:
+    print('other if')
+
+print('Fora do if')
