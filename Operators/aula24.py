d8604a8cca41fcc6d016e6b93c6b5664b1aee82e
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-28 10:31:57 -0300

    feat: exercise with Not In

--- Operators/aula24.py
+++ Operators/aula24.py
@@ -3,3 +3,12 @@ name = 'Juan'
 print(name[2])
 
 print('Ju' in name)
+
+nome = input('Name:')
+find = input('Find: ')
+
+
+if find in name: 
+    print(f'{find} está em {nome}')
+else:
+    print(f'{find} não está em {nome}')
