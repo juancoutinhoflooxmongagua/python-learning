e1fc54155c02175cb04938d9a49b717fb1a95a04
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-28 14:48:04 -0300

    feat: Continue + While in code

---
+++ Operators/aula40.py
@@ -0,0 +1,14 @@
+count = 0 
+
+while count <= 10:
+    count += 1 
+
+    if count == 6:
+        print('Não tem 6')
+        continue
+    print(count)
+
+
+    if count == 20:
+        break
+print('Acabou') 
