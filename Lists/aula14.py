e5b263bef9d140cfd0a8fdb06ccfd8fcaae5cf35
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-30 15:01:57 -0300

    feat: refactor code and aprimoring

--- Lists/aula14.py
+++ Lists/aula14.py
@@ -2,63 +2,40 @@
 
 # Bem vindo a consulta de CPF
 
+# 244.430.538-83
+
 lista = []
 
-cpf = input('Digite seu CPF: ')
-
-lista = cpf
-
+lista = cpf = input('Digite seu CPF: ')
 cpf_formated = lista.replace('.', "").replace('-', "")
-int_cpf = int(cpf_formated)
-list_of_cpf = [int_cpf]
-is_int = type(int_cpf)
-
-print(f'CPF FORMATADO PRA INT: {int_cpf}')
-
-# print(is_int)
-
-# INICIANDO
 
 tuple_cpf = tuple((cpf_formated))
 
 def sum():
-    n1_cpf = tuple_cpf[0] ; print(f'N1 CPF: {n1_cpf}')
-    n1 = int(n1_cpf) * 10
-   
-    n2_cpf = tuple_cpf[1] ; print(f'N2 CPF: {n2_cpf}')
-    n2 = int(n2_cpf) * 9
-   
-    n3_cpf = tuple_cpf[2] ; print(f'N3 CPF: {n3_cpf}')
-    n3 = int(n3_cpf) * 8
-   
-    n4_cpf = tuple_cpf[3] ; print(f'N4 CPF: {n4_cpf}')
-    n4 = int(n4_cpf) * 7
-   
-    n5_cpf = tuple_cpf[4] ; print(f'N5 CPF: {n5_cpf}')
-    n5 = int(n5_cpf) * 6
+    n1 = int(tuple_cpf[0]) * 10
 
-    n6_cpf = tuple_cpf[5] ; print(f'N6 CPF: {n6_cpf}')
-    n6 = int(n6_cpf) * 5
-   
-    n7_cpf = tuple_cpf[6] ; print(f'N7 CPF: {n7_cpf}')
-    n7 = int(n7_cpf) * 4
-   
-    n8_cpf = tuple_cpf[7] ; print(f'N8 CPF: {n8_cpf}')
-    n8 = int(n8_cpf) * 3
-   
-    n9_cpf = tuple_cpf[8] ; print(f'N9 CPF: {n9_cpf}')
-    n9 = int(n9_cpf) * 2
-    
+    n2 = int(tuple_cpf[1]) * 9
+
+    n3 = int(tuple_cpf[2]) * 8
+
+    n4 = int(tuple_cpf[3]) * 7
+
+    n5 = int(tuple_cpf[4]) * 6
+
+    n6 = int(tuple_cpf[5]) * 5
+
+    n7 = int(tuple_cpf[6]) * 4
+
+    n8 = int(tuple_cpf[7]) * 3
+
+    n9 = int(tuple_cpf[8]) * 2
+
     sum_total = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9
     
-    def_result = sum_total * 10 % 11
-    
-    print(def_result)
+    d1 = sum_total * 10 % 11
 
-    if def_result > 9:
+    if d1 > 9:
         print('O resultado é zero.')
     else:
-        print('O resultado é o valor da conta')
+        print(f'O resultado é o valor da conta {d1}')
 sum()
-
-# 244.430.538-83
